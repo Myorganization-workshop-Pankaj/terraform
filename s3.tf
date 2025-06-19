64bcd73d27b94033ab0d3f33d9f8c32b559b1fc0
pankaj620 <59977514+pankaj620@users.noreply.github.com>
2025-06-19 23:58:38 +0530

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"  # change to your preferred region
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name-123456"  # must be globally unique
+  acl    = "private"
+
+  tags = {
+    Name        = "MyBucket"
+    Environment = "Dev"
+  }
+}
